1f875e0cf58b7ff8086dd3daa5a40c44dd46d4c5
Robotics <robotics@jesuittampa.org>
2020-01-16 17:40:22 -0500

    drive up and running, need to fix some errors still

--- src/main/cpp/input.cpp
+++ src/main/cpp/input.cpp
@@ -5,22 +5,27 @@
 
 //constructor
 input::input(){
-    controller = new Gamepad(1);
+    controller = new Gamepad(0);
     aligner = new limelight();
     drivechain = new drive(1, 2, 3, 4);
 }
 
 void input::update(){
     
+    float* LeftStick = controller->LeftJoystick();
+    float* RightStick = controller->RightJoystick();
+    printf("Left Joystick\n\nX:%f\nY:%f\n\n", LeftStick[0], LeftStick[1]);
+    printf("Right Joystick\n\nX:%f\nY:%f\n\n", RightStick[0], RightStick[1]);
+
     //limelight alignment set to A button
     if (controller->ButtonA()){
         float adjustment = aligner->update();
-        float left = controller->LeftJoystick()[1] + adjustment;
-        float right = controller->RightJoystick()[1] - adjustment;
+        float left = LeftStick[1] + adjustment;
+        float right = RightStick[1] - adjustment;
         drivechain->update(right, left);
-    } else { 
-        float left = controller->LeftJoystick()[1];
-        float right = controller->RightJoystick()[1];
+    } else {
+        float left = LeftStick[1];
+        float right = RightStick[1];
         drivechain->update(right, left);
     }
 }
